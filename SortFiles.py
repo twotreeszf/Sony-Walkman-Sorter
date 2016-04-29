e5d747925cd048f623094392d128b9ee7db7c1cf
张凡 <zhangfan@baby1hr.com>
2016-04-29 16:28:18 +0800

    按中文首字母缩写排序，支持繁体，替换拼音生成方案

--- SortFiles.py
+++ SortFiles.py
@@ -1,81 +1,47 @@
 #coding: utf-8
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 import os
 from os import path
 
 from mutagen.mp3 import EasyMP3 as MP3
 from mutagen.easymp4 import EasyMP4 as MP4
+from pypinyin import lazy_pinyin
 
-def chineseCharacterLetter(str):
-    if str > u'\u9fa5' or str < u'\u4e00':
-        return None
-    str = str.encode('gbk', 'ignore')
-    asc = ord(str[0]) * 256 + ord(str[1]) - 65536
-    if asc >= -20319 and asc <= -20284: 
-        return 'a' 
-    if asc >= -20283 and asc <= -19776: 
-        return 'b' 
-    if asc >= -19775 and asc <= -19219: 
-        return 'c' 
-    if asc >= -19218 and asc <= -18711: 
-        return 'd' 
-    if asc >= -18710 and asc <= -18527: 
-        return 'e' 
-    if asc >= -18526 and asc <= -18240: 
-        return 'f' 
-    if asc >= -18239 and asc <= -17923: 
-        return 'g' 
-    if asc >= -17922 and asc <= -17418: 
-        return 'h' 
-    if asc >= -17417 and asc <= -16475: 
-        return 'j' 
-    if asc >= -16474 and asc <= -16213: 
-        return 'k' 
-    if asc >= -16212 and asc <= -15641: 
-        return 'l' 
-    if asc >= -15640 and asc <= -15166: 
-        return 'm' 
-    if asc >= -15165 and asc <= -14923: 
-        return 'n' 
-    if asc >= -14922 and asc <= -14915: 
-        return 'o' 
-    if asc >= -14914 and asc <= -14631: 
-        return 'p' 
-    if asc >= -14630 and asc <= -14150: 
-        return 'q' 
-    if asc >= -14149 and asc <= -14091: 
-        return 'r' 
-    if asc >= -14090 and asc <= -13119: 
-        return 's' 
-    if asc >= -13118 and asc <= -12839: 
-        return 't' 
-    if asc >= -12838 and asc <= -12557: 
-        return 'w' 
-    if asc >= -12556 and asc <= -11848: 
-        return 'x' 
-    if asc >= -11847 and asc <= -11056: 
-        return 'y' 
-    if asc >= -11055 and asc <= -10247: 
-        return 'z'
+def beginWithCJK(str):
+    if u'\u4e00' <= str[0] <= u'\u9fff':
+        return True
     else:
-        return None
+        return False
+
+def CJKFirstLetters(str):
+    pinyins = lazy_pinyin(str)
+    firstLetters = ''
+    for pinyin in pinyins:
+        firstLetters += pinyin[0]
+
+    return firstLetters
 
 def checkTagMakeSort(path, meta, tagName):
+    sortKey = tagName + 'SORT'
+
+    tagSort = meta.get(sortKey)
+    if tagSort and len(tagSort) and tagSort[0] != '':
+        tagSort = tagSort[0]
+        if not beginWithCJK(tagSort):
+            return False
+
     tag = meta.get(tagName)
     if (not tag) or (not len(tag)) or (tag[0] == ''):
-        print('[warning] dont have [{0}] info'.format(tagName))
+        return False
     else:
         tag = tag[0]
-        letter = chineseCharacterLetter(tag)
-        if not letter:
-            print('[info] {0}: [{1}] is \"{2}\", not chinese'.format(path, tagName, tag))
+        if not beginWithCJK(tag):
+            return False
         else:
-            tagSortName = tagName + 'SORT'
-            tagSort = letter + tag
-            meta[tagSortName] = tagSort
-            print('[info] {0}: processed [{1}] is \"{2}\"'.format(path, tagSortName, tagSort))
+            firstLetters = CJKFirstLetters(tag)
+            meta[sortKey] = firstLetters
+            print('[info] {0}: processed [{1}] is \"{2}\"'.format(path, sortKey, firstLetters))
+            return True
 
 def setSortTags(path):
     ext = os.path.splitext(path)[1]
@@ -87,19 +53,20 @@ def setSortTags(path):
         elif ext == '.m4a':
             meta = MP4(path)
         else:
-            print('[info] {0} not audio file'.format(path))
             return
     except:
         pass
 
     if meta:
+        tagsToProcess = ('ALBUM', 'ALBUMARTIST', 'ARTIST', 'TITLE')
+        save = False
         try:
-            checkTagMakeSort(path, meta, 'ALBUM')
-            checkTagMakeSort(path, meta, 'ALBUMARTIST')
-            checkTagMakeSort(path, meta, 'ARTIST')
-            checkTagMakeSort(path, meta, 'TITLE')
-
-            meta.save()
+            for tag in tagsToProcess:
+                processed = checkTagMakeSort(path, meta, tag)
+                if (processed):
+                    save = True
+            if save:
+                meta.save()
         except:
             pass
 
