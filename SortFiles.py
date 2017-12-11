3ed80d4f1483153c74214c13b6e1b5970161ca94
JZ Han <hanjz96@gmail.com>
2017-12-11 19:33:37 +0800

    Added Japanese Support

--- SortFiles.py
+++ SortFiles.py
@@ -1,10 +1,13 @@
-#coding: utf-8
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 import os
 from os import path
+import logging
+import JKConverter
+
+logging.basicConfig(level=logging.DEBUG)
 
 from mutagen.easyid3 import EasyID3
 EasyID3.RegisterTextKey('ALBUMARTIST', 'TPE2')
@@ -14,19 +17,19 @@ from mutagen.mp3 import EasyMP3 as MP3
 from mutagen.easymp4 import EasyMP4 as MP4
 from mutagen.flac import FLAC
 
-from pypinyin import lazy_pinyin
+from pypinyin import lazy_pinyin, Style
 
 def beginWithCJK(str):
-    if u'\u4e00' <= str[0] <= u'\u9fff':
+    if u'\u2e80' <= str[0] <= u'\u9fff':
         return True
     else:
         return False
 
 def CJKFirstLetters(str):
-    pinyins = lazy_pinyin(str)
+    pinyins = lazy_pinyin(str, style=Style.FIRST_LETTER, errors=returnJapaneseKoreanPinyin)
     firstLetters = ''
     for pinyin in pinyins:
-        firstLetters += pinyin[0]
+        firstLetters += pinyin
 
     return firstLetters
 
@@ -37,19 +40,22 @@ def checkTagMakeSort(path, meta, tagName):
     if tagSort and len(tagSort) and tagSort[0] != '':
         tagSort = tagSort[0]
         if not beginWithCJK(tagSort):
+            logging.debug('Not processing tag {0} because its non-CJK'.format(tagSort))
             return False
 
     tag = meta.get(tagName)
     if (not tag) or (not len(tag)) or (tag[0] == ''):
+        logging.debug('Not processing tag {0} because its tag is invalid or empty'.format(tag))
         return False
     else:
         tag = tag[0]
         if not beginWithCJK(tag):
+            logging.debug('Not processing tag {0} because its non-CJK'.format(tag))
             return False
         else:
             firstLetters = CJKFirstLetters(tag)
             meta[sortKey] = firstLetters
-            print('[info] {0}: processed [{1}] is \"{2}\"'.format(path, sortKey, firstLetters))
+            logging.info('{0}: processed [{1}] is \"{2}\", formally \"{3}\"'.format(path, sortKey, firstLetters, tag))
             return True
 
 def setSortTags(path):
@@ -66,6 +72,7 @@ def setSortTags(path):
         else:
             return
     except:
+        logging.warning('unknown file type:{0}'.format(path))
         pass
 
     if meta:
@@ -78,10 +85,13 @@ def setSortTags(path):
                     save = True
             if save:
                 meta.save()
-        except:
+        except Exception as e:
+            logging.warning('Save tag for {0} failure'.format(path))
+            logging.exception(e)
             pass
 
 def processFilesInFolder(folder):
+    logging.info('processing folder [{0}]'.format(folder))
     dirlist = os.listdir(folder)
     for x in dirlist:
         abspath = path.join(folder, x)
@@ -89,6 +99,33 @@ def processFilesInFolder(folder):
             setSortTags(abspath)
         elif (path.isdir(abspath)):
             processFilesInFolder(abspath)
+
+
+def isEngAndNum(aChar):
+    if aChar == ' ':
+        return True
+    if aChar < '0':
+        return False
+    if aChar <= '9':
+        return True
+    if aChar < 'A':
+        return False
+    if aChar <= 'Z':
+        return True
+    if aChar < 'a':
+        return False
+    if aChar <= 'z':
+        return True
+    return False
+
+
+def returnJapaneseKoreanPinyin(unknownStr):
+    returnedStr = JKConverter.convert(unknownStr)
+    pinyin = ""
+    for returnedChar in returnedStr:
+        if isEngAndNum(returnedChar):
+            pinyin += returnedChar
+    return pinyin
 
 if __name__ == '__main__':
     message = '''
@@ -107,14 +144,13 @@ if __name__ == '__main__':
 
     folder = None
     if len(sys.argv) < 2 :
-        print('[info] no folder path specified, process current folder')
+        logging.info('No folder path specified, will process current folder')
         folder = folder = os.getcwd()
     else:
         folder = sys.argv[1]
         if not path.isdir(folder):
-            print('[error] target folder not exist')
+            logging.error('Target folder does not exist')
             folder = None
 
-    if (folder):
-        print('[info] process folder [{0}]'.format(folder))
+    if folder:
         processFilesInFolder(folder)
